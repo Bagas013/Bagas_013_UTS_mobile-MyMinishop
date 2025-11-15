3a5671672c468b1dc68ce8187d0229eb3a27c9c0
Bagas013 <fbagas75@gmail.com>
2025-11-15 21:33:18 +0700

    Bagas_013_5B1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'pages/daftar_produk.dart';
+import 'pages/profil_mahasiswa.dart';
+import 'pages/daftar_kontak.dart';
+import 'pages/dashboard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +16,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tugas Flutter',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const DaftarProdukPage(), // panggil page dari folder pages
+      home: const DaftarProdukPage(), // halaman awal
+      routes: {
+        '/produk': (context) => const DaftarProdukPage(), //SOAL 1
+        '/profil': (context) => const ProfilMahasiswaPage(), //SOAL 2
+        '/kontak': (context) => const DaftarKontakPage(), // SOAL 3
+        '/dashboard': (context) => const DashboardPage(), // SOAL 4
+      },
     );
   }
 }
